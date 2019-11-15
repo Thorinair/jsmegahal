dac256aaabdc794137ee32dddaa1d6f2e898590b
Thorinair <thorinair@yahoo.com>
2019-11-15 16:29:42 +0100

    Added asterisk to word chars.

--- jsmegahal.js
+++ jsmegahal.js
@@ -24,7 +24,7 @@
     })();
 
     jsMegaHal = (function() {
-        jsMegaHal.prototype.wordRegex = /[^a-zA-Zа-яА-Я0-9:;()><_\-,'\u00C0-\u017F\u00a9\u00ae\u2000-\u3300\ud83c\ud000-\udfff\ud83d\ud000-\udfff\ud83e\ud000-\udfff]+/;
+        jsMegaHal.prototype.wordRegex = /[^a-zA-Zа-яА-Я0-9:;()><_*\-,'\u00C0-\u017F\u00a9\u00ae\u2000-\u3300\ud83c\ud000-\udfff\ud83d\ud000-\udfff\ud83e\ud000-\udfff]+/;
         jsMegaHal.prototype.sentenceRegex = /[!?\.\n]/;
 
         /*
